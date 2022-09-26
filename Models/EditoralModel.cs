2994fdcbfd0428f05545cfd2dfbe5bc728bd2b30
Abdiel Cuevas <61069624+Javier13g@users.noreply.github.com>
2022-09-26 13:01:55 -0400

    Cambios menores

--- Models/EditoralModel.cs
+++ Models/EditoralModel.cs
@@ -1,9 +1,17 @@
-﻿namespace CrudSimple_Editorials.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudSimple_Editorials.Models
 {
     public class EditoralModel
     {
+        [Required]
         public int CodEditorial { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Limite alcanzado")]
         public string NombreEditorial { get; set; } = null!;
+
+        [Required]
         public decimal LatitudUbicacion { get; set; }
         public decimal LongitudUbicacion { get; set; }
     }
